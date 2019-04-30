ce0b1399a99b90c2546e834fe2d5d1e992a375a0
DoNeob <49068885+DoNeob@users.noreply.github.com>
2019-05-01 02:13:46 +0900

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,20 @@
+const clock = document.querySelector("#clock")
+
+function getTime(){
+  const time=new Date();
+  var hour = time.getHours();
+  var minute = time.getMinutes();
+  var second = time.getSeconds();
+
+  if(hour < 10) hour = '0'+hour;
+  if(minute < 10) minute = '0'+minute;
+  if(second < 10) second = '0'+second;
+
+  clock.innerHTML = hour + ":" + minute+ ":" + second;
+}
+
+function init(){
+  setInterval(getTime, 1000);
+}
+
+init();
